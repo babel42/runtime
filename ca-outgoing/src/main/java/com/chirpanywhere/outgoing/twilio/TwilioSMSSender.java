eea6d0133115cc3be7efaffdb3ca71d80fbc4f57
Babu Rao <babel@esolutionsoft.com>
2015-04-12 18:03:56 -0400

    added phone number to Send API

--- ca-outgoing/src/main/java/com/chirpanywhere/outgoing/twilio/TwilioSMSSender.java
+++ ca-outgoing/src/main/java/com/chirpanywhere/outgoing/twilio/TwilioSMSSender.java
@@ -16,12 +16,12 @@ public class TwilioSMSSender {
 	public static final String ACCOUNT_SID = "AC589c5fe4aee0746a2285e718c0d8341e"; 
 	public static final String AUTH_TOKEN = "7476f459573937aec71b04f768881b91"; 
 
-	public void send(String msg) throws TwilioRestException {
+	public void send(String phone, String msg) throws TwilioRestException {
 		TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN); 
 
 		// Build the parameters 
 		List<NameValuePair> params = new ArrayList<NameValuePair>(); 
-		params.add(new BasicNameValuePair("To", "+14045791180")); 
+		params.add(new BasicNameValuePair("To", phone)); 
 		params.add(new BasicNameValuePair("From", "+16789056928")); 
 		params.add(new BasicNameValuePair("Body", msg));   
 
@@ -29,9 +29,4 @@ public class TwilioSMSSender {
 		Message message = messageFactory.create(params); 
 		System.out.println(message.getSid()); 
 	} 
-
-	public static void main(String[] args) throws TwilioRestException {
-		TwilioSMSSender sender = new TwilioSMSSender();
-		sender.send("Hi from java");
-	}
 }
