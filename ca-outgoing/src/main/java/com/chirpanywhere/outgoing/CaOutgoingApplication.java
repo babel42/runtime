a780ca942ac796bfa29fb84132208759f25c4c84
Babu Rao <babel@esolutionsoft.com>
2015-04-12 16:55:17 -0400

    Initial checkin

---
+++ ca-outgoing/src/main/java/com/chirpanywhere/outgoing/CaOutgoingApplication.java
@@ -0,0 +1,12 @@
+package com.chirpanywhere.outgoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaOutgoingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaOutgoingApplication.class, args);
+    }
+}
