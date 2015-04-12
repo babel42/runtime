ef6bbfc87197bcaac1d59ab1e8789c2874aa2c85
Babu Rao <baburao@esolutionsoft.com>
2015-04-12 17:01:04 -0400

    Addre request params

--- ca-ingest/src/main/java/com/chirpanywhere/ingest/IngestController.java
+++ ca-ingest/src/main/java/com/chirpanywhere/ingest/IngestController.java
@@ -4,27 +4,36 @@ import java.util.HashMap;
 import java.util.UUID;
 import java.util.concurrent.Future;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chirpanywhere.framework.errorhandling.CAInvalidMessageException;
 import com.chirpanywhere.framework.errorhandling.InvalidClusterConfigException;
 import com.chirpanywhere.framework.integration.Messaging.KafkaIntegration;
 import com.chirpanywhere.framework.utils.Constants;
+
+
+
+
 //import org.apache.kafka.clients.producer;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
 @RestController
+@RequestMapping("/demo")
 public class IngestController {
 	KafkaIntegration kafka = new KafkaIntegration();
 
-	@RequestMapping("/")
+	@RequestMapping(method=RequestMethod.GET)
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
 
-	@RequestMapping("/send/demo/wechat")
-	public String publish(String msg) {
+	@RequestMapping(value="/ott/wechat/send",method=RequestMethod.POST)
+	public String publish(@RequestBody String msg) {
 		String uuid = UUID.randomUUID().toString();
 		kafka.initMessagingSystem();
 		// TODO: this object creation is not needed. Optimize this later
@@ -32,6 +41,7 @@ public class IngestController {
 		msgMap.put(Constants.TOPIC, Constants.KAFKA_DEMO_WECHAT);
 		msgMap.put(Constants.MESSAGE_VALUE, "Message key: " + uuid);
 		msgMap.put(Constants.MESSAGE_KEY, uuid);
+		msgMap.put(Constants.PAYLOAD, msg);
 		try {
 			Future<RecordMetadata> rm = kafka.publishMessage(msgMap);
 		} catch (CAInvalidMessageException e) {
@@ -44,8 +54,10 @@ public class IngestController {
 		return null;
 	}
 
-	@RequestMapping("/get/demo/wechat")
-	public void consume() {
-		kafka.consumeMessage("A", "kafka_demo_wechat", 2);
+	@RequestMapping(value="/ott/wechat/get",method=RequestMethod.GET)
+	public void consume(@RequestParam("group") String group, 
+			@RequestParam("topic") String topic, @RequestParam("numOfThreads") int numOfThreads) {
+		kafka.consumeMessage(group, topic, numOfThreads);
+		return;
 	}
 }
