fbfb0874c21f147f68d082f411039c22c3c20968
Babu Rao <babel@esolutionsoft.com>
2015-04-12 13:27:48 -0400

    Checking in start script for runtime

---
+++ ca-deploy/application-launch/start_babel42runtime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker-compose up -d
+docker-compose scale kafka=3
+docker exec -t dockercompose_kafka_1 /opt/kafka_2.10-0.8.2.0/bin/kafka-topics.sh --zookeeper zk:2181 --topic kafka_demo_wechat --partitions 3 --replication-factor 2 --create
+docker exec -t dockercompose_kafka_1 /opt/kafka_2.10-0.8.2.0/bin/kafka-topics.sh --zookeeper zk:2181 --topic kafka_demo_sms --partitions 3 --replication-factor 2 --create
+docker exec -t dockercompose_kafka_1 /opt/kafka_2.10-0.8.2.0/bin/kafka-topics.sh --zookeeper zk:2181 --list
+docker exec -t dockercompose_kafka_1 /opt/kafka_2.10-0.8.2.0/bin/kafka-topics.sh --zookeeper zk:2181 --topic kafka_demo_wechat --describe
+docker exec -t dockercompose_kafka_1 /opt/kafka_2.10-0.8.2.0/bin/kafka-topics.sh --zookeeper zk:2181 --topic kafka_demo_sms --describe
+docker run -p "8080:8080" --link="dockercompose_kafka_1:kafka" -t babel42/runtime:v1 
