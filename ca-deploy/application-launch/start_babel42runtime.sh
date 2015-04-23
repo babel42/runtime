f7942abedb5a9985a6f845d3a7cc99dbe04101f1
Babu Rao <babel@esolutionsoft.com>
2015-04-23 01:43:01 -0400

    removed starting our app

--- ca-deploy/application-launch/start_babel42runtime.sh
+++ ca-deploy/application-launch/start_babel42runtime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d -p=8080:8080 -v ${HOME}/git/runtime/ca-ingest/target:/usr/local/tomcat/webapps/chirp --name tomcat8 tomcat
+#docker run -d -p=8080:8080 -v ${HOME}/git/runtime/ca-ingest/target:/usr/local/tomcat/webapps/chirp --name tomcat8 tomcat
 docker-compose up -d
 #docker-compose scale kafka=3
 docker exec -t applicationlaunch_kafka_1 /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh --zookeeper zk:2181 --topic kafka_demo_wechat --partitions 3 --replication-factor 2 --create
