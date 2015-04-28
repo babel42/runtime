eb31dbc7a629fec60a781c18f16d13bca0a4191a
Babu Rao <baburao@esolutionsoft.com>
2015-04-27 23:53:39 -0400

    Enhanced with context object

--- ca-framework/src/main/java/com/ca/framework/errorhandling/CAException.java
+++ ca-framework/src/main/java/com/ca/framework/errorhandling/CAException.java
@@ -1,7 +1,10 @@
 package com.ca.framework.errorhandling;
 
+import com.ca.utils.data.IValueObject;
+
 public class CAException extends Throwable {
 	private CAError error = null;
+	private IValueObject context;
 
 	public CAException(Throwable ex, String sourceMsg, CAError err) {
 		super(sourceMsg, ex);
@@ -12,4 +15,26 @@ public class CAException extends Throwable {
 		super(sourceMsg, ex);
 		error.setErrorCode(errorCode);
 	}
+	public CAException(Throwable ex, String sourceMsg, CAError err, IValueObject cxt) {
+		super(sourceMsg, ex);
+		error=err;
+		context = cxt;
+	}
+
+	public CAError getError() {
+		return error;
+	}
+
+	public void setError(CAError error) {
+		this.error = error;
+	}
+
+	public IValueObject getContext() {
+		return context;
+	}
+
+	public void setContext(IValueObject context) {
+		this.context = context;
+	}
+	
 }
