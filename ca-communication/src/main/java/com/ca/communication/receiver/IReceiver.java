5a45387f767007ba44d5ed6b11fbe78aae15b02e
Babu Rao <baburao@esolutionsoft.com>
2015-05-08 23:37:59 -0400

    Deleting all old stuff

--- ca-communication/src/main/java/com/ca/communication/receiver/IReceiver.java
+++
@@ -1,8 +0,0 @@
-package com.ca.communication.receiver;
-
-import com.ca.framework.errorhandling.CAException;
-import com.ca.utils.data.IValueObject;
-
-public interface IReceiver {
-	IValueObject execute(IValueObject vo) throws CAException;
-}
